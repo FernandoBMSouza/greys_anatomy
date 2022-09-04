2d7cc1b3f92db4fe3e14b2b7563c75c1a67c2c71
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-09-04 15:13:40 -0300

    detalhes

--- Greys Anatomy/Assets/Scripts/Target.cs
+++ Greys Anatomy/Assets/Scripts/Target.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    private Vector2 followSpot; //variavel armazena a posicao que foi clicada
-    [SerializeField] private float movementSpeed = 5f; //velocidade do player
-    //[SerializeField] private float perspectiveScale = 1f, scaleRatio = 1.5f;
+    private Vector2 followSpot;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float perspectiveScale = 1f, scaleRatio = 1.5f;
     private Animator animator;
-    private Vector2 stuckDistanceCheck;
+
     private void Start()
     {
-        followSpot = transform.position; //ao iniciar a posicao a ser seguida recebe a posicao inicial pra começar parado
+        followSpot = transform.position; 
         animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); //criando variavel que recebe a posição do ponto da tela em que foi clicado
-        if (Input.GetMouseButtonDown(0)) //se clicar com botão esquerdo
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
         { 
-            followSpot = new Vector2(mousePosition.x, mousePosition.y); //o ponto a ser seguido recebe x e y do local em que foi clicado
+            followSpot = new Vector2(mousePosition.x, mousePosition.y);
         }
-        transform.position = Vector2.MoveTowards(transform.position, followSpot, movementSpeed * Time.deltaTime); //posicao do player recebe o follow spot, mas para não teleportar usa o MoveTowards e multiplica pelo delta time pra suavizar a movimentação
-        //AdjustPerspective();
+        transform.position = Vector2.MoveTowards(transform.position, followSpot, movementSpeed * Time.deltaTime);
+        AdjustPerspective();
         UpdateAnimation();
     }
 
@@ -31,22 +31,13 @@ public class Target : MonoBehaviour
     {
         float distance = Vector2.Distance(transform.position, followSpot);
 
-        /*
-        if (Vector2.Distance(stuckDistanceCheck, transform.position) == 0) 
-        { 
-            animator.SetFloat("distance", 0f); 
-            return; 
-        }
-        */
-
         animator.SetFloat("distance", distance);
 
-        if(distance > 0.01)
+        if(distance > 0.01f)
         {
             Vector3 direction = transform.position - new Vector3(followSpot.x, followSpot.y, transform.position.y);
             float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
             animator.SetFloat("angle", angle);
-            //stuckDistanceCheck = transform.position;
         }
     }
 
@@ -55,14 +46,11 @@ public class Target : MonoBehaviour
         followSpot = transform.position;
     }
 
-    /*
     private void AdjustPerspective()
     {
         Vector3 scale = transform.localScale;
         scale.x = perspectiveScale * (scaleRatio - transform.position.y);
         scale.y = perspectiveScale * (scaleRatio - transform.position.y);
         transform.localScale = scale;
-        //Debug.Log(perspectiveScale / transform.position.y * scaleRatio);
     }
-    */
 }
