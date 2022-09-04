de18bfac1cd525a7a0d7b58f3e2cee4b5db14861
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-09-04 16:16:39 -0300

    greyss

--- Greys Anatomy/Assets/Scripts/Target.cs
+++ Greys Anatomy/Assets/Scripts/Target.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Target : MonoBehaviour
 {
     private Vector2 followSpot;
+    private Vector2 stuckDistanceCheck;
+
     [SerializeField] private float movementSpeed = 5f;
     [SerializeField] private float perspectiveScale = 1f, scaleRatio = 1.5f;
+    
     private Animator animator;
+    private NavMeshAgent agent;
 
     private void Start()
     {
         followSpot = transform.position; 
         animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     private void Update()
@@ -22,15 +30,16 @@ public class Target : MonoBehaviour
         { 
             followSpot = new Vector2(mousePosition.x, mousePosition.y);
         }
-        transform.position = Vector2.MoveTowards(transform.position, followSpot, movementSpeed * Time.deltaTime);
-        AdjustPerspective();
+        agent.SetDestination(new Vector3(followSpot.x, followSpot.y, transform.position.z));
+       
+        //AdjustPerspective();
         UpdateAnimation();
     }
 
     private void UpdateAnimation()
     {
         float distance = Vector2.Distance(transform.position, followSpot);
-
+        if (Vector2.Distance(stuckDistanceCheck, transform.position) == 0) { animator.SetFloat("distance", 0f); return; }
         animator.SetFloat("distance", distance);
 
         if(distance > 0.01f)
@@ -38,6 +47,7 @@ public class Target : MonoBehaviour
             Vector3 direction = transform.position - new Vector3(followSpot.x, followSpot.y, transform.position.y);
             float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
             animator.SetFloat("angle", angle);
+            stuckDistanceCheck = transform.position;
         }
     }
 
@@ -53,4 +63,10 @@ public class Target : MonoBehaviour
         scale.y = perspectiveScale * (scaleRatio - transform.position.y);
         transform.localScale = scale;
     }
+
+    private void AdjustSortingLayer()
+    {
+
+    }
+
 }
